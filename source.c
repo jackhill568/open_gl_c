1bef2e81af58b51f5281902848649cc66fe55023
Jack <jackhill9090@gmail.com>
2025-02-05 22:12:24 +0000

    added seperate files for shaders

--- source.c
+++ source.c
@@ -19,28 +19,6 @@ static const Vertex vertices[3] =
     { {  0.5f, 0.5f }, { -0.5f, -0.5f, 0.f } },
     { {   0.5f,  0.5f }, { 0.5f, -0.5f, 0.f } }
 };
- 
-static const char* vertex_shader_text =
-"#version 330\n"
-"uniform mat4 MVP;\n"
-"in vec3 vCol;\n"
-"in vec2 vPos;\n"
-"out vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-"    color = vCol;\n"
-"}\n";
- 
-static const char* fragment_shader_text =
-"#version 330\n"
-"in vec3 color;\n"
-"out vec4 fragment;\n"
-"void main()\n"
-"{\n"
-"    fragment = vec4(color, 1.0);\n"
-"}\n";
-
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
